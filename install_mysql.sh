6cbce036a2a842877a2e58acd426e37c3281b72d
sankar.boro <sankar.boro@yahoo.com>
2022-07-26 09:40:41 +0530

    mysql

---
+++ install_mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade
+sudo apt-get install mysql-server
+sudo apt install mysql-client
+echo 
+VERSION=$(mysql --version)
+echo $VERSION
+
+# fix mysql_secure_installation
+
+# sudo mysql
+# ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by 'password';
